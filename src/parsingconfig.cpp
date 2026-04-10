51eb8285f5a6b77bdb6f50ff22488b6e34bfa155
bemma <emmabou2611@gmail.com>
2026-04-10 16:04:35 +0200

    add some directives

--- src/parsingconfig.cpp
+++ src/parsingconfig.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <cctype>
 #include <stack>
+#include <sys/stat.h>
 
 // permet de savoir dans quel bloc je suis je ne l'utilise que pour validate pour l'instant
 enum State {
@@ -120,21 +121,65 @@ bool validateListen(std::vector<std::string> args) {
 				nb_pv++;
 			}
 		}
-		// if (nb_pv != 2 && nb_pv != 0)
-		// {
-			
-		// 	return (false);
-		// }
 	}
+	return (true);
+}
+
+bool validateRoot(std::vector<std::string> args) {
+	if (args.size() != 1)
+	{
+		std::cout << "pas bon nb d'argument" << std::endl;
+		return (false);
+	}
+	if (args[0].empty())
+		return (false);
+	if (access(args[0].c_str(), F_OK) == -1)
+	{
+		std::cout << "le dossier n'existe pas" << std::endl;
+		return (false);
+	}
+	if (access(args[0].c_str(), R_OK) == -1)
+	{
+		std::cout << "je n'arrive pas a lire " << std::endl;
+		return (false);
+	}
+	struct stat sb;
+	if (stat(args[0].c_str(), &sb) == -1)
+	{
+		std::cout << "stat pas bon" << std::endl;
+		return (false);
+	}
+	if (!S_ISDIR(sb.st_mode))
+	{
+		std::cout << "C'est pas un dossier" << std::endl;
+		return (false);
+	}
+	std::cout << "--le path est bon!" << std::endl;
+	return (true);
+}
+
+bool validateClientMaxBodySize(std::vector<std::string> args) {
+	if (args.size() != 1)
+		return (false);
+	if (args[0].empty())
+		return (false);
+	size_t i = 0;
+	while (i < args[0].size() && isdigit(args[0][i]))
+		i++;
+	if (i < args[0].size())
+		return (false);
 	return (true);
 }
 
 bool validateSpecificDirective(std::string name, std::vector<std::string> args) {
 	if (name == "listen")
-	{
-		std::cout << "Je suis un listen" << std::endl;
 		return (validateListen(args));
-	}
+	else if (name == "root")
+		return (validateRoot(args));
+	else if (name == "server_name")
+		return (true);
+	else if (name == "client_max_body_size")
+		return (validateClientMaxBodySize(args));
 	return (false);
 	
 }
@@ -160,7 +205,7 @@ bool validateOneDirective(std::vector<std::string> tokens, size_t& i, State stat
 	}
 	if (args.size() == 0)
 		return (false);
-	if (i >= tokens.size())
+	if (i >= tokens.size() || tokens[i] != ";")
 		return (false);
 	if (validateSpecificDirective(name, args) == false)
 		return (false);
@@ -218,7 +263,7 @@ bool validateStructure(std::vector<std::string> tokens) {
 		else if (isSimpleDirective(tokens[i]) == true)
 		{
 
-			std::cout << "         Je suis sur une directive!" << std::endl;
+			// std::cout << "         Je suis sur une directive!" << std::endl;
 			if (validateOneDirective(tokens, i, state) == false)
 				return (false);
 		}
