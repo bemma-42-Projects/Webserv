b307773d10e62ed8cd676f97e7fb4c95e3e38a35
bemma <emmabou2611@gmail.com>
2026-04-15 04:41:40 +0200

    Update LocationConfig.hpp

--- src/LocationConfig.hpp
+++ src/LocationConfig.hpp
@@ -5,20 +5,36 @@
 #include <vector>
 #include <iostream>
 #include <map>
+#include <utility>
 
 class LocationConfig {
+public:
 
-	std::string uri;
-	std::string root;
-	std::vector<std::string> index;
-	bool autoindex;
-	std::map<int, std::string> return_;
-	std::vector<std::string> allowed_methods;
-	bool allowed_upload;
-	std::string upload_path;
-	std::map<std::string, std::string> cgi;
-	size_t client_max_body_size;
-	std::map<int, std::string> error_page;
+	LocationConfig();
+	const std::string&							getUri() const;
+	const std::string&							getRoot() const;
+	const std::vector<std::string>&				getIndex() const;
+	bool										getAutoindex() const;
+	const std::pair<int, std::string>&			getReturn() const;
+	const std::vector<std::string>&				getAllowedMethods() const;
+	bool										getAllowedUpload() const;
+	const std::string&							getUploadPath() const;
+	const std::map<std::string, std::string>&	getCgi() const;
+	size_t										getClientMaxBodySize() const;
+	const std::map<int, std::string>&			getErrorPage() const;
+
+private:
+	std::string							uri_;
+	std::string							root_;
+	std::vector<std::string>			index_;
+	bool								autoindex_;
+	std::pair<int, std::string>			return_;
+	std::vector<std::string>			allowed_methods_;
+	bool								allowed_upload_;
+	std::string							upload_path_;
+	std::map<std::string, std::string>	cgi_;
+	size_t								client_max_body_size_;
+	std::map<int, std::string>			error_page_;
 };
 
 //il manquerait config cgi
