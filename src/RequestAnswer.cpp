4af565f3f3dc0004eabff8cc32e637fdbc05a45b
sakamoto-42 <julien66140@hotmail.fr>
2026-04-16 13:15:12 +0200

    envp premier test ok

--- src/RequestAnswer.cpp
+++ src/RequestAnswer.cpp
@@ -1,8 +1,10 @@
 #include "RequestAnswer.hpp"
 #include <iostream>
-#include <fcntl.h>    // pour open
-#include <unistd.h>   // pour read, close
-#include <sys/stat.h> // pour stat
+#include <fcntl.h>		// pour open
+#include <unistd.h>		// pour read, close, fork et execve
+#include <sys/stat.h>	// pour stat
+//#include <sys/wait.h>	// pour waitpid
+//#include <cstdlib>		// pour exit
 #include "Config.hpp"
 #include <dirent.h>
 #include <sstream>
@@ -374,6 +376,8 @@ bool	RequestAnswer::isCgi()
 // on est ici si c'est un CGI
 int	RequestAnswer::methodCGI()
 {
+	char	**envp = this->getEnvp();
+	(void)envp;
 	return (1);
 }
 
@@ -384,10 +388,64 @@ char		**RequestAnswer::getEnvp()
 {
 	std::vector<std::string>	env;
 
-	env.push_back("REQUEST_METHOD=" + request_.getMethod());
-	env.push_back("SCRIPT_FILENAME=" + request_.getPath());
-	env.push_back("SERVER_PROTOCOL=HTTP/1.1");
+	// la version du protocole CGI utilisée par le serveur
+	// CGI/1.1
 	env.push_back("GATEWAY_INTERFACE=CGI/1.1");
+
+	// la version du protocole HTTP utilisée par la requête
+	// HTTP/1.1
+	env.push_back("SERVER_PROTOCOL=HTTP/1.1");
+
+	// la méthode utilisée par le client
+	// exemples : GET ou POST ou DELETE
+	env.push_back("REQUEST_METHOD=" + request_.getMethod());
+	
+	// l'URL complète demandée par le client (path + paramètres)
+	// exemple : /cgi-bin/script.php?user=test
+	//env.push_back("REQUEST_URI=" + request_.getRequestUri());
+	
+	// le chemin absolu vers le script
+	// exemple : /home/julien/Webserv/cgi-bin/script.php
+	env.push_back("SCRIPT_FILENAME=" + request_.getPath());
+	
+	// le chemin virtuel vers le script (l'URI sans la query string)
+	// exemple : /cgi-bin/script.php
+	env.push_back("SCRIPT_NAME=" + request_.getUrlPath());
+	
+	// tout ce qui se trouve après le "?" dans l'URL (query string)
+	// exemple : user=test&age=25 
+	//env.push_back("QUERY_STRING=" + request_.getQueryString());
+	
+	// le type de contenu envoyé dans le body
+	// indispensable pour les formulaires POST
+	// exemple : application/x-www-form-urlencoded
+	//env.push_back("CONTENT_TYPE=" + request_.getContentType());
+	
+	// la taille en octets des données du body
+	// indispensable pour que PHP lise le POST
+	// exemple : 15
+	//std::stringstream	ss_len;
+	//ss_len << request_.getContentLength();
+	//env.push_back("CONTENT_LENGTH=" + ss_len.str());
+	
+	// le nom de domaine du serveur
+	// ou l'adresse IP utilisée pour l'atteindre
+	// exemples : localhost ou 127.0.0.1
+	//env.push_back("SERVER_NAME=" + request_.getHost());
+	
+	// le port sur lequel le serveur écoute
+	// exemple : 8080
+	//std::stringstream	ss_port;
+	//ss_port << request_.getPort();
+	//env.push_back("SERVER_PORT=" + ss_port.str());
+	
+	// l'adresse IP du client qui a fait la requête
+	// exemple : 192.168.1.10
+	//env.push_back("REMOTE_ADDR=" + request_.getClientIP());
+	
+	// code de statut de redirection
+	// exigé par php-cgi pour s'exécuter (mesure de sécurité)
+	// 200
 	env.push_back("REDIRECT_STATUS=200");
 
 	this->addHeadersToEnv(env);
@@ -402,6 +460,15 @@ char		**RequestAnswer::getEnvp()
 		i++;
 	}
 	envp[env.size()] = NULL;
+
+	// --- DEBUG ---
+    std::cout << "\n=== DEBUG CGI ENV ===" << std::endl;
+    for (int j = 0; envp[j] != NULL; j++)
+    {
+        std::cout << "envp[" << j << "] : " << envp[j] << std::endl;
+    }
+    std::cout << "=====================\n" << std::endl;
+    // ------------------------------------------------------
 	return (envp);
 }
 
@@ -415,8 +482,31 @@ void	RequestAnswer::addHeadersToEnv(std::vector<std::string>& env_vector)
 {
 	std::map<std::string, std::string>	headers = request_.getHeaders();
 
-	std::map<std::string, std::string>::iterator	it;
+	std::map<std::string, std::string>::iterator	it = headers.begin();
 
-	// TODO
-	(void)env_vector;
+	while (it != headers.end())
+	{
+		std::string	key = it->first;
+		std::string	value = it->second;
+
+		if (key == "Content-Type" || key == "Content-Length")
+		{
+			++it;
+			continue;
+		}
+
+		std::string	env_key = "HTTP_";
+
+		size_t	i = 0;
+		while (i < key.length())
+		{
+			if (key[i] == '-')
+				env_key += '_';
+			else
+				env_key += toupper(key[i]);
+			i++;
+		}
+		env_vector.push_back(env_key + "=" + value);
+		++it;
+	}
 }
